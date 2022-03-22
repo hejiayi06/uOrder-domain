757dc6d52aca32eeafc44f2f1bea32c1bafe451a
Maikesi <82232645+UP-max@users.noreply.github.com>
2022-03-21 20:36:05 -0400

    fix: bugs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
-import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { Loader } from '@googlemaps/js-api-loader';
+import { fromEvent, Subscription } from 'rxjs';
 import { environment } from 'src/environments/environment';
 import { UpdateService } from './services/service-workers/update.service';
 
@@ -11,9 +11,33 @@ import { UpdateService } from './services/service-workers/update.service';
 })
 export class AppComponent {
   title = 'uOrder';
+  private touchStartSubscription!: Subscription;
+  private touchEndSubscription!: Subscription;
   constructor(private updateServe: UpdateService) {
     this.updateServe;
     this.googleMap();
+    this.doubleClick();
+  }
+  private doubleClick(): void {
+    this.touchStartSubscription && this.touchStartSubscription.unsubscribe();
+    this.touchEndSubscription && this.touchEndSubscription.unsubscribe();
+    let lastTouch = 0;
+    this.touchStartSubscription = fromEvent(window, 'touchstart').subscribe(
+      (event) => {
+        if ((event as TouchEvent).touches.length > 1) {
+          event.preventDefault();
+        }
+      }
+    );
+    this.touchEndSubscription = fromEvent(window, 'touchend').subscribe(
+      (event) => {
+        const now = new Date().getTime();
+        if (now - lastTouch <= 300) {
+          event.preventDefault();
+          lastTouch = now;
+        }
+      }
+    );
   }
   googleMap(): void {
     new Loader({
